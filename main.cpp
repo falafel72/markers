dbffc953c0ac87d47b3f5743a4305f176b3739dd
Falafel72 <ksh717@student.cis.edu.hk>
2016-08-16 17:19:45 +0800

    determined angle of rotation

--- main.cpp
+++ main.cpp
@@ -31,25 +31,61 @@ bool clockwiseComp(Point2f p1, Point2f p2) { //starting from bottom right
 	return d1 > d2;
 }
 
-// bool isMarker(Mat &marker) {
+bool isMarker(Mat &markerCode) {
+	Mat markBin;
+	markerCode.convertTo(markBin,CV_8UC1);
+	Mat mask(markBin.rows, markBin.cols, CV_8UC1, Scalar(255));
+	Mat_<unsigned char>::iterator it = mask.begin<unsigned char>();
+	Mat_<unsigned char>::iterator itend = mask.end<unsigned char>();
+	int counter = 0;
 
-// }
+	for (int counter = 0; it != itend; ++counter, ++it) {
+		int row = counter/mask.rows;
+		int col = counter % mask.cols;
+		if(row == 0 || row == mask.rows-1 || row == mask.rows/2 - 1 || (mask.rows % 2 == 0 && row == mask.rows/2) ) {
+			(*it) = 0;
+		}
+		if(col == 0 || col == mask.cols-1 || col == mask.cols/2 - 1 || (mask.cols % 2 == 0 && col == mask.cols/2)) {
+			(*it) = 0;
+		}
+		//cout << counter << " " << *it << endl;	
+	}
+	Mat bitOr = Mat(markerCode.size(), CV_8UC1);
+	//cout << mask << endl;
+	bitwise_or(markBin,mask,bitOr);
+	//cout << markBin << endl;
+	//cout << mask << endl;
+	//cout << bitOr << endl;
+	//cout << (mask!=bitOr) << endl;
+	
+	return countNonZero(mask!=bitOr) == 0;
+}
 
 Mat getCode(Mat &marker, int markerSize=14) {
 	Mat code(Size(markerSize,markerSize), CV_8UC1);
-	Mat_<Vec3b>::iterator it = code.begin<Vec3b>(); 
-	Scalar testavg(128,0,0,0);
+	Mat_<unsigned char>::iterator it = code.begin<unsigned char>(); 
 	for(int row = 0; row < markerSize; row++) {
 		for(int col = 0; col < markerSize; col++) {
 			Mat region = marker(Rect((col * marker.cols/markerSize), (row * marker.rows/markerSize), marker.cols/markerSize,marker.rows/markerSize));
 			Scalar output; 
-			(*it) = (mean(region)[0] < 128) ? 1 : 0;
+			(*it) = (mean(region)[0] < 90) ? 255 : 0;
 			it++;
 		}
-		//cout << count << endl;
 	}
-	
+	//code.convertTo(code,);
 	return code;
+}
+
+int getQuadrant(Mat &marker) {
+	Mat temp;
+	for(int i = 0; i < 4; i++) {
+		temp = imread("templates/" + to_string(i) + ".bmp", CV_8UC1);
+		if(countNonZero(temp!=marker) == 0) {
+			return (i >= 90) ? 180-i : i ;
+		}
+	}
+
+	return -1;
 }
 
 void visSections(Mat &marker, int markerSize = 14) {
@@ -79,6 +115,13 @@ void denoisify(Mat &in, Mat &out) {
 	erode (in,out,Mat(Size(3,3), CV_8U));
 }
 
+float getAngle(Point2f &a, Point2f &b) {
+	//float gradient = (b.y - a.y)/(b.x-a.x);
+	float angleRad = atan2(a.y - b.y, a.x - b.x);
+	float angleDeg = angleRad * 180/M_PI;
+	return angleDeg;
+}
+
 vector<string> getCalibFiles() {
 	vector<string> filenames;
 	DIR *dir;
@@ -102,7 +145,7 @@ void calibrateCamera(CameraCalibrator &c, Size &imageSize) {
 	Size boardSize = Size(7,5);
 	c.addChessboardPoints(filenames,boardSize); 
 	double reprojerr = c.calibrate(imageSize);
-	cout << reprojerr << endl;	
+	//cout << reprojerr << endl;	
 	//return c.getCameraMatrix();
 }
 
@@ -123,41 +166,39 @@ int main(int argc, char **argv) {
 		c.setCameraMatrix(cameraMat);
 		c.setDistCoeffs(distCoeffs);
 	}
-	//cout << "test" << endl;
 
-	//double reprojerr = c.calibrate(imageSize);
-	
+	//undistort image	
 	Mat image = c.remap(i); 
 
+	//minimise noise with erosion
 	Mat denoised;
 	denoisify(image,denoised);
 
-	//imshow("image", denoised);
 
-
+	//threshold image
 	int thresh = 40;
 
 	Mat thresholded;
 	threshold(image,thresholded,90,255,THRESH_BINARY_INV);
-	//adaptiveThreshold(denoised,thresholded,255,ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,5,3);
-	//Canny(denoised, thresholded, 20,255);
 	imshow("thresholded",thresholded);
+	
+	//perform opening of image to allow for better edge detection
 	Mat element = Mat(Size(3,3),CV_8U);
 	Mat element2 = Mat(Size(5,5),CV_8U);
 	erode(thresholded,thresholded,element);
 	dilate(thresholded,thresholded,element2);
-	//imshow("thresholded", thresholded);
-
+	
+	//get contours
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy; 
 	findContours(thresholded, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
 	
-
 	Mat drawing = Mat::zeros(thresholded.size(), CV_8UC3);
 	Mat drawing2 = image.clone();
 
 	drawContours(drawing,contours,-1,Scalar(255,255,255));
-
+	
+	//find suitable polygons that could be markers
 	vector<vector<Point> > polygons; 
 	vector<Rect> boundingRects;
 	for(int i = 0; i < contours.size() ; i++) {
@@ -175,37 +216,25 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	imshow("contours",drawing);
-	//imshow("polygons", drawing2);
+	//imshow("contours",drawing);
+	imshow("polygons", drawing2);
+	
 
+	//copies all potential markers to their own images
 	vector<Mat> marks; 
 	for(int i = 0; i < boundingRects.size(); i++) {
 		Mat imageROI = image(Rect(boundingRects[i].x,boundingRects[i].y,boundingRects[i].width,boundingRects[i].height));
-		//Mat marker(imageROI.rows,imageROI.cols,CV_8U);	
 		Mat marker;
 		imageROI.copyTo(marker);	
-		//threshold(marker,marker,0,255,THRESH_BINARY_INV | THRESH_OTSU);
 		marks.push_back(marker);
-		//imshow(to_string(i),marks[i]);
 	}
 
 	for(int i = 0; i < marks.size(); i++) {
 		//threshold to get contours
 		
-		//denoisify(marks[i],marks[i]);
-		//cout << "world" << endl;
-
-		//imshow("marker"+to_string(i),marks[i]);
-
-		//Mat edgeThresh;
-		//adaptiveThreshold(marks[i],edgeThresh,255,ADAPTIVE_THRESH_MEAN_C,THRESH_BINARY,5,3);
-
 		Mat markThresh;
 		threshold(marks[i],markThresh,90,255,THRESH_BINARY_INV);
-		//erode(markThresh,markThresh,Mat());
-		//cout << "hello" << endl;
 		Mat markThresh2 = markThresh.clone();
-		//imshow("thresh" + to_string(i),markThresh);
 
 
 		vector<vector<Point> > markContours;
@@ -217,8 +246,6 @@ int main(int argc, char **argv) {
 
 		drawContours(markdraw,markContours,-1,Scalar(255,255,255));
 
-		//imshow(to_string(i),markdraw);
-		
 
 		vector<Point> markPoly; 
 		vector<Point> markContour;
@@ -236,38 +263,54 @@ int main(int argc, char **argv) {
 				}
 			}
 		}
-
+		
 		imshow("poly"+to_string(i),markdraw);
-		//cout << markContour << endl;
+
+		//if no polygons detected, move to the next "marker"
 		if(markContour.size() == 0) {
 			cerr << "Region invalid" << endl;
 			continue;
 		}
+
+		//get center of polygon
 	  	Moments m = moments(markContour);
 		center = Point(m.m10/m.m00, m.m01/m.m00);
 		vector<Point2f> markPolyf;
 	 	Mat(markPoly).copyTo(markPolyf);
-			  
+		//sort corners clockwise	  
 		sort(markPolyf.begin(), markPolyf.end(), clockwiseComp);
-
+		
+		//get width and height of marker image
 		vector<int> dimensions = getDimensions(markPolyf);	
-
+		
+		//order rectangle corners clockwise (bottom right first)
 		vector<Point2f> markRect;
 		markRect.push_back(Point2f(dimensions[0],dimensions[1]));
 		markRect.push_back(Point2f(dimensions[0],0));
 		markRect.push_back(Point2f(0,0));
 		markRect.push_back(Point2f(0, dimensions[1]));
 
-		//polylines(markThresh, markPoly, true, Scalar(0), 1, 8);
 
-		     //get transformation matrix and wrap perspective
+		//get transformation matrix and wrap perspective
 		Mat transform = getPerspectiveTransform(markPolyf,markRect);
 		Mat warped;
 		warpPerspective(markThresh2,warped,transform,Size(dimensions[0], dimensions[1]));
-		visSections(warped);
-		imshow(to_string(i),warped);
 
-		cout << getCode(warped) << endl;
+		//draw lines
+		visSections(warped);
+
+		//only display if it is valid
+		Mat code = getCode(warped);
+		if(isMarker(code)) 
+			imshow(to_string(i),warped);	
+		
+		int quad = getQuadrant(code);
+		float offsetAngle = getAngle(markPolyf[1], markPolyf[2]);
+		cout << offsetAngle << endl;
+		float angle = 90*quad - offsetAngle;
+		if(angle >= 360) angle -= 360;
+		if(angle < 0) angle += 360;
+		cout << "rotation: " << angle << endl;
 	}
 	waitKey();
 	return 0;
